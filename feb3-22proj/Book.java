72f82f0c785a5c9968951da227470538a1d6285b
Artur <vru11ra@gmail.com>
2016-02-16 15:39:12 -0500

    sync book.java temp
    book.java incomplete

---
+++ feb3-22proj/Book.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Artur
+ *
+ */
+public class Book {
+	private String book_name, author, category, status;
+	private long isbn;
+	public Book(long isbn, String book_name, String author, String category, String status){
+		
+	}
+	public String toString(){
+		return ("ISBN ID ="+isbn+" Book Name ="+book_name+" Author ="+author+" Category ="+category+" Status ="+status);
+	}
+	
+}
+//Books.txt: Contains "ISBN ID", "Book Name", "Author", "Category" (i.e., adventure, romance, textbook, etc.), "Status" (i.e., New, Excellent, Good, Bad etc.)
