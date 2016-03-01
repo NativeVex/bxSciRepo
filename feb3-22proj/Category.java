cc9a84c7b21640da2c4494de0e0379e90810cfd6
Artur <vru11ra@gmail.com>
2016-02-29 20:26:29 -0500

    fixed merge issues, justin's code is still pretty broken
    you might want to start using method declarations and constructors, also your code most likely should belong to borrower as a pre-defined method

--- feb3-22proj/Category.java
+++ feb3-22proj/Category.java
@@ -1,9 +1,10 @@
 //tried my best...By Justin :)
 //alright, so this code should be able to identify what the user is asking for
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Category {
   ArrayList<String>fullList= new ArrayList<String>();
-  Scanner bob = new Scanner();
+  Scanner bob = new Scanner(System.in);
   System.out.println("Please enter an ISBN number, bookname, catagory, or keywordfor the book(s) that you would like:");
   String pokemon = bob.next().toLowerCase();//alright so we are going to make everythin lower case so that java dosent give us any errors. 
   String gundam = "Books.txt";
@@ -21,29 +22,4 @@ public class Category {
   }
   return fullList;
 }
-=======
-//tried my best...By Justin :)
-//alright, so this code should be able to identify what the user is asking for
-import java.util.Scanner;
-public class Category {
-  ArrayList<String>fullList= new ArrayList<String>();
-  Scanner bob = new Scanner();
-  System.out.println("Please enter an ISBN number, bookname, catagory, or keywordfor the book(s) that you would like:");
-  String pokemon = bob.next().toLowerCase();
-  String gundam = "Books.txt";
-  ArrayList<String> catalogue = fetch(gundam);
-  for(int i=0; i < catalogue.size(); i++){
-    String line = catalogue.get(i);
-    String[] s1 = line.split(" ");
-    for(String s : s1){
-        if(pokemon.equals(s)){
-          fullList.add(line);
-        }
-        else
-          break;//is this the correct way?
-    }
-  }
-  return fullList;
-}
-
   
